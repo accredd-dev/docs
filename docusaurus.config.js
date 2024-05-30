8a5241a4303fcdd19451299912450044a6d51e24
Tommy <hoang.tommy12@gmail.com>
2024-05-30 13:41:33 -0700

    removed blog directory, set blog to false in config file, removed blog from nav

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,13 +48,16 @@ const config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
+        blog: false,
+        /*
+        {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
+        */
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -80,7 +83,7 @@ const config = {
             position: 'left',
             label: 'Docs',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          //{to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://accredd.com',
             label: 'Accredd Home',
