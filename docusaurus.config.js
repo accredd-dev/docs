9332ac9e03d1c36f115bcd0778e0fa10daf8ccb0
Tommy <hoang.tommy12@gmail.com>
2024-05-30 13:54:43 -0700

    revert blog changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,16 +48,13 @@ const config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: false,
-        /*
-        {
+        blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        */
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -83,7 +80,7 @@ const config = {
             position: 'left',
             label: 'Docs',
           },
-          //{to: '/blog', label: 'Blog', position: 'left'},
+          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://accredd.com',
             label: 'Accredd Home',
